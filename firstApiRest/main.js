ca8bf4e49e1495013b4bbe1a9b8aa277b9d3e838
Daniel Romero <daanmaro99june@gmail.com>
2022-09-17 13:28:44 -0500

    Ending the fetch use

--- firstApiRest/main.js
+++ firstApiRest/main.js
@@ -2,4 +2,9 @@ console.log('howdy');
 
 const URL = 'https://api.thedogapi.com/v1/images/search';
 
-fetch()
+fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+        const img = document.querySelector('img');
+        img.src = data[0].url;
+    });
