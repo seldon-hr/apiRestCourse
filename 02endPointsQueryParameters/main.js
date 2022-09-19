42b045a34e0a75714d9f9bc2105ad6ccfa88faea
Daniel Romero <daanmaro99june@gmail.com>
2022-09-18 21:56:02 -0500

    Using query parameters

--- 02endPointsQueryParameters/main.js
+++ 02endPointsQueryParameters/main.js
@@ -1,12 +1,28 @@
-const API = 'https://api.thedogapi.com/v1/images/search';
+const API = 'https://api.thedogapi.com/v1/images/search?limit=3';
+/* 
+Endpoints
+    Estos son los lugares o categorias de nuestras apis. e.g.
+    La url de nuestra API es https://api.thedogapi.com/v1/
+    Y un endpoint sería este: images/search
 
+Query parameters
+    Consultas en est caso la iniciamos con ?
+    y si buscamos otra  con &
+*/
 
 async function refresh() {
     const res = await fetch(API);
     const data = await res.json();
+    
+    console.log(data);
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    // img.src = data[0].url;
 
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+    img1.src = data[0].url
+    img2.src = data[1].url
+    img3.src = data[2].url
     
 }
 
