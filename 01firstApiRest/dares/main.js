9e8a62d9fa3461d76392b98618154a1c0c447dce
Daniel Romero <daanmaro99june@gmail.com>
2022-09-19 18:49:19 -0500

    Comments added of async awati function

--- 01firstApiRest/dares/main.js
+++ 01firstApiRest/dares/main.js
@@ -8,4 +8,11 @@ async function refreshcat() {
     img.src = cat[0].url;    
 }
 
+/* 
+    En l sentido más práctico, hacemos una petición fetch, con async await, para esto, primero tenemos una respuesta que es 'response', de allí obtenemos los datos a emplear con la url 'API'
+    después tenemos la data, en este caso cat, así obtenemos y lo pasamos a tipo json, y ahora si podemos emplear la data a nuestra imagen por una manipulación del DOM, dónde obtenemos un array
+    y accedemos a su data.
+*/
+
+
 refreshcat();
