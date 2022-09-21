71916f0ba22d4098b479af9fa91c6b73b802791e
Daniel Romero <daanmaro99june@gmail.com>
2022-09-21 14:02:53 -0500

    Using layout to make a structure

---
+++ Layout/main.js
@@ -0,0 +1,29 @@
+const API = 'https://api.thedogapi.com/v1/images/search?limit=3&apikey=live_VOYMcAfRNwYhJxe31OJ1fntESm8JSYSaoOaYx4mB63hcIrYtmR5fhMtx6YrCW92i';
+/* 
+Endpoints
+    Estos son los lugares o categorias de nuestras apis. e.g.
+    La url de nuestra API es https://api.thedogapi.com/v1/
+    Y un endpoint sería este: images/search
+
+Query parameters
+    Consultas en est caso la iniciamos con ?
+    y si buscamos otra  con &
+*/
+
+async function refresh() {
+    const res = await fetch(API);
+    const data = await res.json();
+    
+    console.log(data);
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    // img.src = data[0].url;
+
+    img1.src = data[0].url
+    img2.src = data[1].url
+    img3.src = data[2].url
+    
+}
+
+refresh();
