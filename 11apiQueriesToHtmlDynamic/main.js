a82f88545bf2b367243cbb3c47ed1ccb182a3ad7
Daniel Romero <daanmaro99june@gmail.com>
2022-11-09 19:43:32 -0600

    Guardar doggos en favoritos, pero también mostralos.

--- 11apiQueriesToHtmlDynamic/main.js
+++ 11apiQueriesToHtmlDynamic/main.js
@@ -1,14 +1,7 @@
-// const API_URL_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=2';
-// Just Api
-
 //URL de la API
 const API = 'https://api.thedogapi.com/v1/';
 const RANDOM = 'images/search';
 const LIMIT_RANDOM = '?limit=2'
-
-/* Dentro de la api con los query parameters se indica el número del tamaño, es decir
-    nuestra búsqueda esta reducida a solo dos. */
-
 
 //API Key
 // const API_KEY = 'live_VOYMcAfRNwYhJxe31OJ1fntESm8JSYSaoOaYx4mB63hcIrYtmR5fhMtx6YrCW92i';
@@ -18,12 +11,6 @@ const API_KEY = 'live_8o81iddRPKsNMkNDSMMYR7j5m2dUochsy69F2Qb33Q9MNBuVMPxfv8zB7m
 //URL for Favourites
 const FAVOURITES = 'favourites';
 
-// const API = 'https://api.thedogapi.com/v1/images/search?limit=2&api_key=live_8o81iddRPKsNMkNDSMMYR7j5m2dUochsy69F2Qb33Q9MNBuVMPxfv8zB7mW5b14c';
-// const API_URL_FAVOURITES = 'https://api.thedogapi.com/v1/favourites'
-
-
-
-
 
 
 const spanError = document.getElementById('error');
@@ -32,12 +19,9 @@ const spanError = document.getElementById('error');
 
 
 async function loadRandomDogs() {
-    // const res = await fetch(`${API_URL_RANDOM}&apiKey=${API_KEY}`);
     const res = await fetch(API+RANDOM+LIMIT_RANDOM);
     const data = await res.json();
- /*    console.log('Random Dogs')
-    console.log(res.status); //Con esto estamos probando que es lo que esta haciendo bajo el objeto json recibido.
-    console.log(data); */
+
     
    
     if (res.status !== 200) {
@@ -45,23 +29,27 @@ async function loadRandomDogs() {
     } else {
         const img1 = document.getElementById('img1');
         const img2 = document.getElementById('img2');
+        const btn1 = document.getElementById('btn1');
+        const btn2 = document.getElementById('btn2');
         
     
         img1.src = data[0].url
         img2.src = data[1].url
+
+        btn1.onclick = () => saveFavouriteDog(data[0].id);
+        btn2.onclick = () => saveFavouriteDog(data[1].id);
     }
     
 }
 
 async function loadFavouriteDogs() {
-    /* Load some dogs via headers, since it's easier to have the request about the data */
     const res = await fetch(API+FAVOURITES, {
         method: 'GET',
         headers: {
             'x-api-key': API_KEY,
         },
     });
-    console.log(res.status);
+    // console.log(res.status);
     
     const data = await res.json();
     console.log('Favorites Dogs')
@@ -70,22 +58,38 @@ async function loadFavouriteDogs() {
     if (res.status !== 200) {
         spanError.innerHTML = `Hubo un error ${res.status} ${data.message}`;
     } else {
+        /* Load dog images in favourites section */
+        data.forEach(doggo => {
+            const section = document.getElementById('favoritesDogs');
+            const article = document.createElement('article');
+            const img = document.createElement('img');
+            const btn = document.createElement('button');
+            const btnText = document.createTextNode('Get out Doggo of Favourites'); //allows create text for our html nodes
 
+
+            btn.appendChild(btnText);
+            img.src = doggo.image.url;
+            img.width = 300;
+
+            article.appendChild(img);
+            article.appendChild(btn);
+            section.appendChild(article)
+        })
     }
     
 }
 
 
 
-async function saveFavouritesDogs() {
+async function saveFavouriteDog(id) {
     const res = await fetch(API+FAVOURITES, {
         method: 'POST',
         headers: {
             'x-api-key': API_KEY,
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ //mandar en un formato estándar
-            image_id: 'R1b5gB11p'
+        body: JSON.stringify({ 
+            image_id: id
         }),
     });
 
@@ -102,4 +106,3 @@ async function saveFavouritesDogs() {
 
 loadRandomDogs();
 loadFavouriteDogs();
-// saveFavouritesDogs();
