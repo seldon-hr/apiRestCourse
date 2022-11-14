87f04042aba9c16bf174bc0bbf59b9bce54c1b75
Daniel Romero <daanmaro99june@gmail.com>
2022-11-14 16:07:53 -0600

    Viendo como funciona fetch()

---
+++ useFetchAndItsCode/onlyPromise/main.js
@@ -0,0 +1,16 @@
+const API = 'https://api.thecatapi.com/v1/images/search';
+
+
+
+const newFetch = new Request(API);
+
+newFetch.json();
+console.log(newFetch);
+
+/* 
+    Steps
+    -Read Uso de Fetc()
+    -Request
+    -Fetch
+    -Response()
+*/
