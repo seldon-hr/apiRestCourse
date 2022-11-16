d7a66cafcbb3e50f143077e664b9721249e8391c
Daniel Romero <daanmaro99june@gmail.com>
2022-11-16 12:36:46 -0600

    Usando fetch() en funciones de tipo expersión.

--- useFetchAndItsCode/promiseOnBrowser/main.js
+++ useFetchAndItsCode/promiseOnBrowser/main.js
@@ -18,12 +18,53 @@ function fetchDataPromise(urlAPI) {
     estamos usando directamente a fetch para poder obtener esta data que obtenemos de las API.
     esto del código de arriba con una función creada y aquí abajo llamando directo con fetch() */
 
-fetch(API)
+/* fetch(API)
     .then(res => res.json())
     .then(data => {
         console.log(data);
         
         //Solo para tener buenas prácticas
+        const section = document.querySelector('section');
+        section.innerHTML = ""
+
+        const h3 = document.createElement('h3');
+        const h3Text = document.createTextNode('Random Cats')
+        h3.appendChild(h3Text);
+        section.appendChild(h3)
+
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+
+        img.src = data[0].url;
+        img.width = 300;
+        
+        article.appendChild(img);
+        section.appendChild(article)
+
+
+
+    })
+
+    .catch(error => console.log(error))
+    .finally(()=> console.log('Finalizo la extracción de datos')) */
+
+
+/* 
+    Estamos asignado la función a una constante, y pasandola por tipo de función
+    en JS, es decir, 
+    *es función por expresión.
+    dónde guardamos una función en una variable, para así ejecutar la variable como si fuera
+    una función. Mientras que arriba solo estabamos llamando fetch() en crudo, y allí mismo ejecutandoto todo
+
+*/
+
+    const request = fetch(API);
+
+    request
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        
         const section = document.querySelector('section');
         section.innerHTML = ""
 
