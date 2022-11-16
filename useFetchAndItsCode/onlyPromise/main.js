1218c7615f410bce802a4bb8c29e6a712bd5f662
Daniel Romero <daanmaro99june@gmail.com>
2022-11-16 13:50:26 -0600

    ¿Cómo funciona fetch()?, Usa response, headers, request

--- useFetchAndItsCode/onlyPromise/main.js
+++ useFetchAndItsCode/onlyPromise/main.js
@@ -4,8 +4,22 @@ const API = 'https://api.thecatapi.com/v1/images/search';
 
 const newFetch = new Request(API);
 
-newFetch.json();
+// newFetch.json();
+// console.log(newFetch);
+// newFetch
+const myURL = newFetch.url;
+const myMehod = newFetch.method;
+
 console.log(newFetch);
+console.log(newFetch.headers);
+
+fetch(newFetch)
+    .then(res => res.json())
+    .then(blob => {
+        console.log(blob);
+    })
+
+
 
 /* 
     Steps
