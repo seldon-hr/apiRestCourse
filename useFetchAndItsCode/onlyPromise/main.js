f366a547da388904cb2b360286f3c17958886b1a
Daniel Romero <daanmaro99june@gmail.com>
2022-11-22 12:14:30 -0600

    Pasos de fetch()

--- useFetchAndItsCode/onlyPromise/main.js
+++ useFetchAndItsCode/onlyPromise/main.js
@@ -12,12 +12,22 @@ const myMehod = newFetch.method;
 
 console.log(newFetch);
 console.log(newFetch.headers);
+console.log(myMehod);
 
 fetch(newFetch)
-    .then(res => res.json())
+    .then(res => res.json()) 
     .then(blob => {
         console.log(blob);
     })
+
+    /* 
+        fetch()
+        *Promesa de fetch es resuelta por then()
+        * este espera un objeto Response y ese objeto nos permite usar la función json() (en este caso)
+        * para resolver la data que viene dentro y se vulve a resolver con un then() y es transformada
+        * esa data, esto cuando usamos then de las promesas.
+        
+    */
 
 
 
