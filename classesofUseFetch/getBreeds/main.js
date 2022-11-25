7cfecbccb39696de04d554c2f5a07be112c92809
Daniel Romero <daanmaro99june@gmail.com>
2022-11-24 22:53:21 -0600

    Haciendo base para la consulta

--- classesofUseFetch/getBreeds/main.js
+++ classesofUseFetch/getBreeds/main.js
@@ -1,8 +1,10 @@
 // URL
 const API = 'https://api.thedogapi.com/v1/';
-const BREEDS = 'breeds';
+const BREEDS = 'breeds/search';
 const LIMIT = '?limit=3'
 
+
+const QUERY = 'Afghan';
 const API_KEY = 'live_8o81iddRPKsNMkNDSMMYR7j5m2dUochsy69F2Qb33Q9MNBuVMPxfv8zB7mW5b14c';
 const HEADERS = {
     method: 'POST',
@@ -10,13 +12,16 @@ const HEADERS = {
         'x-api-key': API_KEY,
         'Content-Type': 'application/json',
     },
+    body: JSON.stringify({
+        query: 'Afghan',
+    }),
 }
 
 
-console.log(`${API}${BREEDS}${LIMIT}`)
+console.log(`${API}${BREEDS}${LIMIT}?q=${QUERY}`)
 
 async function searchBreed() {
-    const res = await fetch(`${API}${BREEDS}${LIMIT}`, HEADERS);
+    const res = await fetch(`${API}${BREEDS}${LIMIT}?q=${QUERY}`, HEADERS);
     const data = await res.json();
 
     if(res.status !== 200){
@@ -25,25 +30,27 @@ async function searchBreed() {
 
         console.log(data);
 
-        const section = document.getElementById('breedsDoggos');
-        data.forEach(breed => {
-            const article = document.createElement('article');
-            const breedName = document.createElement('h5');
-            const breedNameText = document.createTextNode(breed.name);
-            // console.log(breedNameText)
-            const img = document.createElement('img');
+
+
+        // const section = document.getElementById('breedsDoggos');
+        // data.forEach(breed => {
+        //     const article = document.createElement('article');
+        //     const breedName = document.createElement('h5');
+        //     const breedNameText = document.createTextNode(breed.name);
+        //     // console.log(breedNameText)
+        //     const img = document.createElement('img');
 
 
         
-            img.src = breed.image.url;
-            img.width = 300;
-            breedName.appendChild(breedNameText);
+        //     img.src = breed.image.url;
+        //     img.width = 300;
+        //     breedName.appendChild(breedNameText);
 
-            article.appendChild(breedName);
-            article.appendChild(img);
-            section.appendChild(article);
+        //     article.appendChild(breedName);
+        //     article.appendChild(img);
+        //     section.appendChild(article);
 
-        });
+        // });
 
     }
     
