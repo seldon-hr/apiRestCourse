fb870c4b269553f4f25826e44b5620d85627787b
Daniel Romero <daanmaro99june@gmail.com>
2022-11-24 21:53:38 -0600

    Use of how to search something in web

--- classesofUseFetch/getBreeds/search/main.js
+++ classesofUseFetch/getBreeds/search/main.js
@@ -5,22 +5,21 @@ const LIMIT = '?limit=3'
 
 const API_KEY = 'live_8o81iddRPKsNMkNDSMMYR7j5m2dUochsy69F2Qb33Q9MNBuVMPxfv8zB7mW5b14c';
 const HEADERS = {
-    method: 'POST',
+    method: 'GET',
     headers: {
-        'x-api-key': API_KEY,
-        'Content-Type': 'application/json',
+        'x-api-key': API,
     },
 }
 
 
 console.log(`${API}${BREEDS}${LIMIT}`)
 
-async function searchBreed() {
+async function loadBreeds() {
     const res = await fetch(`${API}${BREEDS}${LIMIT}`, HEADERS);
     const data = await res.json();
 
     if(res.status !== 200){
-        console.log(`We have an errror ${res.status} ${data.message}`);
+        console.log(`We have an errro ${res.status} ${data.message}`);
     } else {
 
         console.log(data);
@@ -49,5 +48,5 @@ async function searchBreed() {
     
 }
 
-searchBreed();
+loadBreeds();
 
