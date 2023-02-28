478ea9a7dbaec8da09671fbfe8dd6e991483b39e
Daniel Romero <daanmaro99june@gmail.com>
2023-02-27 18:09:58 -0600

    Fixed the dares of the first consult, in the carpet "01firstApirRest", and when you was used an arrow function to make a reques API, you have to use a try cath within in."

--- classesofUseFetch/01firstApiRest/main.js
+++ classesofUseFetch/01firstApiRest/main.js
@@ -12,21 +12,41 @@ const URL = 'https://api.thedogapi.com/v1/images/search';
 // }
 
 
-async function fetchData(urlApi) {
-    const response = await fetch(urlApi);
-    const data = await response.json();
+/* async function fetchData(urlApi) {
+    console.log(data);
     return data;
-}
+} */
 
-const refreshDoggo = async (urlApi) => {
 
-    const doggo = await fetchData(urlApi);
-
-    const img = document.querySelector('img');
+//*It's seems working using first the word async
+// async function refreshDoggo(){
+//     const response = await fetch(URL);
+//     const doggo = await response.json();
+    
+//     // const doggo = await fetchData(urlApi);
+//     console.log(doggo)
+//     const img = document.querySelector('img');
         
-    img.src = doggo[0].url;
+//     img.src = doggo[0].url;
    
-}
+// }
+
+/* 
+
+Here, we have the same function, but to use in the arrow function, and we could use the code
+try catch to capture a erro
+*/
+const refreshDoggo = async () => {
+    try {
+      const response = await fetch(URL);
+      const doggo = await response.json();
+      const img = document.querySelector('img');
+      img.src = doggo[0].url;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
 
 refreshDoggo(URL);
 // anotherFunction(URL);
